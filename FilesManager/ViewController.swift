9af0c037d49bb802a93a97ab259a9d9bc237544e
Maria Holubieva <holubeva.synergetica@gmail.com>
2020-01-13 11:51:50 +0200

    initial

---
+++ FilesManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FilesManager
+//
+//  Created by Maria Holubieva on 13.01.2020.
+//  Copyright © 2020 Maria Holubieva. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
