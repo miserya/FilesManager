9f0c28b8ac2a785185976609cc52cfee26104491
Maria Holubieva <holubeva.synergetica@gmail.com>
2020-01-15 17:34:09 +0200

    added files removing from the list

--- DataLayer/Services/FilesManager/FilesServiceImpl.swift
+++ DataLayer/Services/FilesManager/FilesServiceImpl.swift
@@ -11,31 +11,37 @@ import Combine
 class FilesServiceImpl: FilesService {
 
     @UserDefault(key: "files_manager_used_files", defaultValue: [])
-    private var files: [File]
+    private var filesList: [File]
 
     private let fileManager = FileManager.default
 
     func getFiles() -> AnyPublisher<[File], Error> {
-        return Publishers.Sequence(sequence: [files]).eraseToAnyPublisher()
+        return Publishers.Sequence(sequence: [filesList]).eraseToAnyPublisher()
     }
 
     func add(filesAt urls: [URL]) -> AnyPublisher<Void, Error> {
         do {
-            let newFilesList = try urls.map { (url: URL) -> File in
+            let newFilesList = try urls.compactMap { (url: URL) -> File? in
+                guard !filesList.contains(where: { $0.location == url }) else { return nil }
+
                 let attributes = try NSDictionary(dictionary: fileManager.attributesOfItem(atPath: url.path))
                 return File(name: url.lastPathComponent,
                             size: attributes.fileSize(),
                             location: url)
             }
-            files.append(contentsOf: newFilesList)
+            filesList.append(contentsOf: newFilesList)
             return Publishers.Sequence(sequence: [()]).eraseToAnyPublisher()
+            
         } catch {
             return Publishers.Fail(outputType: Void.self, failure: error).eraseToAnyPublisher()
         }
     }
 
     func remove(files: [File]) -> AnyPublisher<Void, Error> {
-        return Publishers.Fail(outputType: Void.self, failure: DataLayerError.notImplemented).eraseToAnyPublisher()
+        filesList.removeAll { (file: File) -> Bool in
+            files.contains(where: { $0.location == file.location })
+        }
+        return Publishers.Sequence(sequence: [()]).eraseToAnyPublisher()
     }
 
     func duplicate(files: [File]) -> AnyPublisher<[File], Error> {
